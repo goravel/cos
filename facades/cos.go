fc74e056f786429a2c2b6152f13fcc5c81886aac
maimaiti <maimaiti@archeros.com>
2025-05-12 16:34:09 +0800

    feat: [#597] add support for package install/uninstall via artisan commands

--- facades/cos.go
+++ facades/cos.go
@@ -1,19 +1,16 @@
 package facades
 
 import (
-	"log"
-
 	"github.com/goravel/framework/contracts/filesystem"
 
 	"github.com/goravel/cos"
 )
 
-func Cos(disk string) filesystem.Driver {
+func Cos(disk string) (filesystem.Driver, error) {
 	instance, err := cos.App.MakeWith(cos.Binding, map[string]any{"disk": disk})
 	if err != nil {
-		log.Fatalln(err)
-		return nil
+		return nil, err
 	}
 
-	return instance.(*cos.Cos)
+	return instance.(*cos.Cos), nil
 }
