0e16a8178ddff72f8d5e3a4bdcb0093abb74ef8b
Bowen <hwbrzzl@gmail.com>
2025-07-01 09:35:35 +0800

    fix typo

--- setup/setup.go
+++ setup/setup.go
@@ -31,8 +31,8 @@ func main() {
 		).
 		Uninstall(
 			modify.GoFile(path.Config("app.go")).
-				Find(match.Imports()).Modify(modify.RemoveImport(packages.GetModulePath())).
-				Find(match.Providers()).Modify(modify.Unregister("&cos.ServiceProvider{}")),
+				Find(match.Providers()).Modify(modify.Unregister("&cos.ServiceProvider{}")).
+				Find(match.Imports()).Modify(modify.RemoveImport(packages.GetModulePath())),
 			modify.GoFile(path.Config("filesystems.go")).
 				Find(match.Config("filesystems.disks")).Modify(modify.RemoveConfig("cos")).
 				Find(match.Imports()).Modify(modify.RemoveImport("github.com/goravel/framework/contracts/filesystem"), modify.RemoveImport("github.com/goravel/cos/facades", "cosfacades")),
